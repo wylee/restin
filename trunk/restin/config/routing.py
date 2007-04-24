b8d16ef04f2cb6f74a59ad6ae441b7284df20ec3
wyatt.lee.baldwin <wyatt.lee.baldwin@213ae585-922e-0410-bd61-4358ed8d4160>
2007-04-24 11:04:36 +0000

    routing.py: moved 'manual' map.resource generation to a function, resource

--- trunk/restin/config/routing.py
+++ trunk/restin/config/routing.py
@@ -5,6 +5,11 @@ import os
 from routes import Mapper
 
 
+GET_condition = dict(method=['GET'])
+POST_condition = dict(method=['POST'])
+PUT_condition = dict(method=['PUT'])
+DELETE_condition = dict(method=['DELETE'])
+
 def make_map(global_conf={}, app_conf={}):
     root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     map = Mapper(directory=os.path.join(root_path, 'controllers'))
@@ -15,37 +20,48 @@ def make_map(global_conf={}, app_conf={}):
 
     map.resource('application', 'applications')
 
-    GET_condition = dict(method=['GET'])
-    POST_condition = dict(method=['POST'])
-    PUT_condition = dict(method=['PUT'])
-    DELETE_condition = dict(method=['DELETE'])
-
-    route = 'applications/:application_id/:controller/:entity_name'
-    
-    kw = dict(action='create', conditions=POST_condition)
-    map.connect(route, **kw)
-    
-    kw = dict(action='index', conditions=GET_condition)
-    map.connect(route, **kw)
-    map.connect(route + '.:(format)', **kw)
-    
-    kw = dict(action='new', conditions=GET_condition)
-    map.connect(route + '/new', **kw)
-    map.connect(route + '/new.:(format)', **kw)
-    
-    kw = dict(action='update', conditions=PUT_condition)
-    map.connect(route + '/:id', **kw)
-    
-    kw = dict(action='delete', conditions=DELETE_condition)
-    map.connect(route + '/:id', **kw)
-    
-    kw = dict(action='edit', conditions=GET_condition)
-    map.connect(route + '/:(id);edit', **kw)
-    map.connect(route + '/:(id).:(format);edit', **kw)
-
-    kw = dict(action='show', conditions=GET_condition)
-    map.connect(route + '/:id', **kw)
-    map.connect(route + '/:(id).:(format)', **kw)
+    data_route = 'applications/:application_id/:controller/:entity_name'
+    resource(map, data_route)
 
     map.connect('*url', controller='template', action='view')
     return map
+
+def resource(map, base_route):
+    """Manually create the equivalent of the routes ``map.resource`` generates.
+
+    ``map``
+        The Routes ``Mapper`` object
+
+    ``base_route``
+        The route (string) up to and not including the :id, :action, and
+        :format
+
+        For example::
+
+            "applications/:application_id/:controller/:entity_name"
+
+    """
+    kw = dict(action='create', conditions=POST_condition)
+    map.connect(base_route, **kw)
+
+    kw = dict(action='index', conditions=GET_condition)
+    map.connect(base_route, **kw)
+    map.connect(base_route + '.:(format)', **kw)
+
+    kw = dict(action='new', conditions=GET_condition)
+    map.connect(base_route + '/new', **kw)
+    map.connect(base_route + '/new.:(format)', **kw)
+
+    kw = dict(action='update', conditions=PUT_condition)
+    map.connect(base_route + '/:id', **kw)
+
+    kw = dict(action='delete', conditions=DELETE_condition)
+    map.connect(base_route + '/:id', **kw)
+
+    kw = dict(action='edit', conditions=GET_condition)
+    map.connect(base_route + '/:(id);edit', **kw)
+    map.connect(base_route + '/:(id).:(format);edit', **kw)
+
+    kw = dict(action='show', conditions=GET_condition)
+    map.connect(base_route + '/:id', **kw)
+    map.connect(base_route + '/:(id).:(format)', **kw)
