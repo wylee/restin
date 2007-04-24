fbb38ea09d29ad7bd8ee75e30897fe6b8fa01876
wyatt.lee.baldwin <wyatt.lee.baldwin@213ae585-922e-0410-bd61-4358ed8d4160>
2007-04-24 14:21:04 +0000

    Added initial support for navigating between applications/models/data.
    * restin/models/__init__.py
      * Added a ``model_entities`` property to ``Application``, which fetches
        entity classes from an application's model module
      * Added an empty ``Entity`` class (a class representing entities, metameta)
    
    * restin/config/routing.py
      * Added a route for /applications/:app_id/model
    
    * restin/controllers/data.py
      * Set ``model`` on ``self`` as ``_model``
      * Set ``parent_resource`` names
    
    * restin/lib/base.py
      * removed ``model_registry`` and associated functions (``register_model``
        and ``register_models``); using ``model`` property on ``Application`` 
        instead.

--- restin/lib/base.py
+++ restin/lib/base.py
@@ -4,32 +4,14 @@ from pylons.decorators import jsonify, validate
 from pylons.templating import render, render_response
 from pylons.helpers import abort, redirect_to, etag_cache
 from pylons.i18n import N_, _, ungettext
-from pylons.database import create_engine
 
-import restin.models as model
+import restin.models as restin_model
 import restin.lib.helpers as h
 
 import restler
 
 
-RestController = restler.BaseController(model)
-
-
-model_registry = {'restin': model}
-
-def register_model(app):
-    package_name = app.package_name
-    engine = create_engine(uri=app.dburi, echo=0)
-    restler.init_model(app.model)
-    app.model.metadata.connect(engine)
-    model_registry[package_name] = app.model
-
-def register_models():
-    apps = model.Application.select()
-    for app in apps:
-        register_model(app)
-
-register_models()
+RestController = restler.BaseController(restin_model)
 
 
 class BaseController(WSGIController): pass
