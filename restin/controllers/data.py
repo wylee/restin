cad3afd680c00e0e9d9af707fafec01f3837f3ee
wyatt.lee.baldwin <wyatt.lee.baldwin@213ae585-922e-0410-bd61-4358ed8d4160>
2007-04-24 07:08:01 +0000

    * In data controller, set member name from m query param if present. If   m isn't in params, see if the collection name ends with an 's', and   strip that 's' to get the member name; if collection name doesn't end with   an 's', make the member name the same as the collection name.
    * Created and added CHANGELOG

--- restin/controllers/data.py
+++ restin/controllers/data.py
@@ -5,10 +5,20 @@ class DataController(restler.BaseController(model)):
     def __init__(self):
         route = request.environ['routes.route']
         route_info = request.environ['pylons.routes_dict']
-        c_name = route_info['collection_name']
-        route.collection_name = c_name
-        route.member_name = c_name[:-1]
+
         application_id = route_info['application_id']
         app = self._get_entity_or_404(model.Application, application_id)
         self.model = model_registry[app.package_name]
+
+        c_name = route_info['collection_name']
+        m_name = request.params.get('m', None)
+        if m_name is None:
+            if c_name.endswith('s'):
+                m_name = c_name[:-1]
+            else:
+                m_name = c_name
+
+        route.member_name = m_name
+        route.collection_name = c_name
+
         super(DataController, self).__init__()
